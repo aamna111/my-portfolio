a8976f619a398599e8723b9fe5be17aef3069f1d
aamna111 <aamnaansari111@gmail.com>
2025-03-17 08:08:00 +0530

    updating changes

--- portfolio/src/components/Footer.tsx
+++ portfolio/src/components/Footer.tsx
@@ -1,32 +1,32 @@
-import { Mail, Linkedin, Github, Phone } from "lucide-react"
+import { Linkedin, Github } from "lucide-react"
 
-export const Footer=()=>{
-    return(
-        <section id="contact" className="py-20 bg-gray-900/50">
-        <div className="max-w-7xl mx-auto px-6 text-center">
-          <h2 className="text-3xl font-bold mb-8">Let's Connect</h2>
-          <p className="text-xl text-gray-400 mb-12 max-w-2xl mx-auto">
-            Available for challenging opportunities. Let's discuss how I can contribute to your team.
-          </p>
-          <div className="flex justify-center gap-6">
-            {[
-              { icon: <Mail size={24} />, label: "Email" },
-              { icon: <Linkedin size={24} />, label: "LinkedIn" },
-              { icon: <Github size={24} />, label: "GitHub" },
-              { icon: <Phone size={24} />, label: "Phone" }
-            ].map((contact, index) => (
-              <a
-                key={index}
-                href="#"
-                className="p-4 bg-gray-800 rounded-xl hover:bg-gray-700 transition-colors group"
-              >
-                <div className="text-gray-400 group-hover:text-blue-400 transition-colors">
-                  {contact.icon}
-                </div>
-              </a>
-            ))}
-          </div>
+export const Footer = () => {
+  return (
+    <section id="contact" className="py-20 bg-gray-900/50">
+      <div className="max-w-7xl mx-auto px-6 text-center">
+        <h2 className="text-3xl font-bold mb-8">Let's Connect</h2>
+        <p className="text-xl text-gray-400 mb-12 max-w-2xl mx-auto">
+          Available for challenging opportunities. Let's discuss how I can contribute to your team.
+        </p>
+        <div className="flex justify-center gap-6">
+          {[
+            { icon: <Linkedin size={24} />, label: "LinkedIn", url: "www.linkedin.com/in/aamna-ansari" },
+            { icon: <Github size={24} />, label: "GitHub", url: "https://github.com/aamna111" },
+          ].map((contact, index) => (
+            <a
+              key={index}
+              href={contact.url || "#"}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="p-4 bg-gray-800 rounded-xl hover:bg-gray-700 transition-colors group"
+            >
+              <div className="text-gray-400 group-hover:text-blue-400 transition-colors">
+                {contact.icon}
+              </div>
+            </a>
+          ))}
         </div>
-      </section>
-    )
+      </div>
+    </section>
+  )
 }
--- portfolio/src/data/projects.ts
+++ portfolio/src/data/projects.ts
@@ -1,23 +1,30 @@
 import Movix from "../assets/movix.png"
+import productStorefrom from "../assets/product_store.png"
 
 const projects = [
-    {
-      id: "Movix",
-      description: "Movie platform focused on providing a user-friendly interface for streaming services.",
-      tech: ["React", "TypeScript", "Tailwind CSS", "Node.js"],
-      img: Movix,
-      links: [
-        { type: "GitHub", url: "https://github.com/user/project1" },
-        { type: "Live Demo", url: "https://aamna111.github.io./movix/" }
-      ]
-    },
-    {
-      
-    },
-    {
+  {
+    id: "Movix",
+    description: "Movie platform focused on providing a user-friendly interface for streaming services.",
+    tech: ["React", "TypeScript", "Tailwind CSS" , "Redux"],
+    img: Movix,
+    links: [
+      { type: "GitHub", url: "https://github.com/user/project1" },
+      { type: "Live Demo", url: "https://aamna111.github.io./movix/" }
+    ]
+  },
+  {
+    id: "Product Store",
+    description: "A simple fullstack application that to create, update and delete products in products store.",
+    tech: ["React", "Node js", "Express","Mongo DB", "Zustand"] ,
+    img: productStorefrom,
+    links: [
+      { type: "GitHub", url: "https://github.com/aamna111/product-store" },
+      { type: "Live Demo", url: "https://product-store-vvi7.onrender.com/" }
+    ]
+  },
+  {
 
-    }
-  ];
-  
-  export default projects;
-  
+  }
+];
+
+export default projects;
